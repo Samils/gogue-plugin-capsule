9d61bd282853a8c11a85c10f5a9c0dde6101fb42
Samil <agostinhosaml832@gmail.com>
2022-09-22 22:57:18 +0100

    fix(static::readContainerCapsule): make sure $capsuleDatas is an array

--- Sammy/Packs/Gogue/Transpiler/Capsule/XMLReader/Capsules/Base.php
+++ Sammy/Packs/Gogue/Transpiler/Capsule/XMLReader/Capsules/Base.php
@@ -7,14 +7,36 @@
  * -----------------
  * @package Sammy\Packs\Gogue\Transpiler\Capsule\XMLReader\Capsules
  * - Autoload, application dependencies
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 Ysare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 namespace Sammy\Packs\Gogue\Transpiler\Capsule\XMLReader\Capsules {
   /**
    * Make sure the module base internal class is not
-   * declared in the php global scope defore creating
+   * declared in the php global scope before creating
    * it.
    */
-  if (!trait_exists('Sammy\Packs\Gogue\Transpiler\Capsule\XMLReader\Capsules\Base')){
+  if (!trait_exists ('Sammy\Packs\Gogue\Transpiler\Capsule\XMLReader\Capsules\Base')) {
   /**
    * @trait Base
    * Base internal trait for the
@@ -22,7 +44,7 @@ namespace Sammy\Packs\Gogue\Transpiler\Capsule\XMLReader\Capsules {
    * -
    * This is (in the ils environment)
    * an instance of the php module,
-   * wich should contain the module
+   * which should contain the module
    * core functionalities that should
    * be extended.
    * -
@@ -30,7 +52,6 @@ namespace Sammy\Packs\Gogue\Transpiler\Capsule\XMLReader\Capsules {
    * an 'exts' directory in the module directory
    * and boot it by using the ils directory boot.
    * -
-   * \Samils\dir_boot ('./exts');
    */
   trait Base {
     /**
@@ -68,13 +89,15 @@ namespace Sammy\Packs\Gogue\Transpiler\Capsule\XMLReader\Capsules {
         $signature
       );
 
-      #echo $capsuleDatas['attributes'], "\n\n";
-
       array_push (self::$capsuleStore, $capsuleDatas);
     }
 
     private function readContainerCapsule ($capsuleDatas) {
       $id = count (self::$capsuleStore);
+
+      if (!is_array ($capsuleDatas)) {
+        return;
+      }
 
       $codeSlice = substr (
         $this->code,
@@ -87,13 +110,6 @@ namespace Sammy\Packs\Gogue\Transpiler\Capsule\XMLReader\Capsules {
         "\n%::capsule-code{$id}::\n",
         $this->code
       );
-
-      #$this->code = substr_replace ($this->code, "\n%::capsule-code{$id}::\n",
-      #    $capsuleDatas['init'], $capsuleDatas['end']
-      #);
-
-
-      #exit("FIM \n\n\n\n\n\n". $this->code);
 
       $capsuleBody = $capsuleDatas ['body'];
       $capsuleSignatureDatas = $this->getCapsuleSignatureInPartialCode (
@@ -114,19 +130,8 @@ namespace Sammy\Packs\Gogue\Transpiler\Capsule\XMLReader\Capsules {
         $capsuleDatas ['name'] = trim ($match[2]);
       }
 
-      $capsuleDatas ['attributes'] = $this->readCapsuleAttributes (
-        $capsuleSignatureDatas ['signature']
-      );
+      $capsuleDatas ['attributes'] = $this->readCapsuleAttributes ($capsuleSignatureDatas ['signature']);
 
-
-      #echo "\n\n\n\n\n\n\n\n\n\033[31m$capsuleBody\033[m\n\n\n\n\n\n\n\n\n";
-      #print_r($capsuleSignatureDatas);
-      #print_r($capsuleDatas);
-
-      #echo "\n\n\n\nMANS\n\n\n\n";
-
-      #print_r($capsuleDatas);
-      #echo "\n\n\n\nMANS\n\n\n\n";
       array_push (self::$capsuleStore, $capsuleDatas);
     }
   }}
